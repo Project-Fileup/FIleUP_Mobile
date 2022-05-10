66a1e15520452449b76d5926f03e4547afcb1826
KimKyungWon0519 <gwon080870@gmail.com>
2022-05-10 23:20:21 +0900

    FEAT: Add email text field

--- lib/src/features/presentation/page/sign_in_page.dart
+++ lib/src/features/presentation/page/sign_in_page.dart
@@ -1,6 +1,7 @@
 import 'package:file_up/src/features/presentation/widgets/sign_background_frame.dart';
 import 'package:flutter/material.dart';
 
+import '../../../config/values/app_strings.dart';
 import '../widgets/circle_text_field.dart';
 
 class SignInPage extends SignBackGroundFrame {
@@ -10,7 +11,10 @@ class SignInPage extends SignBackGroundFrame {
   body() {
     return Container(
       child: Column(
-        children: [
+        children: const [
+          CircleTextField(
+            hintText: AppStrings.emailTextField,
+          ),
         ],
       ),
       margin: const EdgeInsets.only(right: 45, left: 45),
