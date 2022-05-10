b439911144df5473a7a28d7118de1875c585c927
KimKyungWon0519 <gwon080870@gmail.com>
2022-05-10 22:48:03 +0900

    ADD: Add sign_background_frame.dart

---
+++ lib/src/features/presentation/widgets/sign_background_frame.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignBackGroundFrame extends StatelessWidget {
+  const SignBackGroundFrame({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
