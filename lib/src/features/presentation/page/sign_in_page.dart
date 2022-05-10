9ce6ff8a3e8aa3b058e13be8fda132a61d1daf8e
KimKyungWon0519 <gwon080870@gmail.com>
2022-05-10 23:08:51 +0900

    FEAT: Add margin in singIn page

--- lib/src/features/presentation/page/sign_in_page.dart
+++ lib/src/features/presentation/page/sign_in_page.dart
@@ -1,11 +1,19 @@
 import 'package:file_up/src/features/presentation/widgets/sign_background_frame.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/circle_text_field.dart';
+
 class SignInPage extends SignBackGroundFrame {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
   body() {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+        ],
+      ),
+      margin: const EdgeInsets.only(right: 45, left: 45),
+    );
   }
 }
