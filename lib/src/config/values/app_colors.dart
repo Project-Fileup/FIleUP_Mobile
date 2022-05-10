6e6be9302ca913d9d29c6bf3193ef0db7a196f0a
KimKyungWon0519 <gwon080870@gmail.com>
2022-05-10 23:17:46 +0900

    FEAT: Add circle text field

---
+++ lib/src/config/values/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primayColor = Color.fromRGBO(89, 130, 200, 1);
+}
---
+++ lib/src/features/presentation/widgets/circle_text_field.dart
@@ -0,0 +1,32 @@
+import 'package:file_up/src/config/values/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CircleTextField extends StatelessWidget {
+  const CircleTextField({
+    Key? key,
+    this.hintText = '',
+  }) : super(key: key);
+
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: hintText,
+        enabledBorder: _border(),
+        focusedBorder: _border(),
+        contentPadding:
+            const EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+      ),
+    );
+  }
+
+  _border() => OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(
+          color: AppColors.primayColor,
+          width: 2,
+        ),
+      );
+}
