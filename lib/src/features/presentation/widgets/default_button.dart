a4c892e153b70914a67232c45b622292de25303d
KimKyungWon0519 <gwon080870@gmail.com>
2022-05-12 22:44:45 +0900

    FEAT: Add elevated button style

--- lib/src/features/presentation/widgets/default_button.dart
+++ lib/src/features/presentation/widgets/default_button.dart
@@ -4,15 +4,28 @@ class DefaultButton extends StatelessWidget {
   const DefaultButton({
     Key? key,
     required this.title,
+    this.width = double.infinity,
+    this.height = double.infinity,
   }) : super(key: key);
 
   final String title;
+  final double width;
+  final double height;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text(title),
+    return SizedBox(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(title),
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+      ),
+      width: width,
+      height: 48,
     );
   }
 }
