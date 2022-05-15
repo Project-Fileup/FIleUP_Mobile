9c80ba85b0583b65df178febe6c8ef9c24188679
KimKyungWon0519 <gwon080870@gmail.com>
2022-05-15 18:23:11 +0900

    CREATE: Create ask sign up

---
+++ lib/src/features/presentation/page/local_widgets/ask_sign_up.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AskSignUp extends StatelessWidget {
+  const AskSignUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
