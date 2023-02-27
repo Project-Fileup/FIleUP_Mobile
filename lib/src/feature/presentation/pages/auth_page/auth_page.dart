8e8088bd8eeea62551d91da28f6d6e03a9c6fce8
kim kyugnwon <gwon080870@gmail.com>
2023-02-27 20:46:57 +0900

    FEAT: Add login page

--- lib/src/feature/presentation/pages/auth_page/auth_page.dart
+++ lib/src/feature/presentation/pages/auth_page/auth_page.dart
@@ -1,10 +1,74 @@
+import 'dart:ui';
+
+import 'package:file_up_mobile/src/feature/presentation/pages/auth_page/local_widgets/base_auth_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: SvgPicture.asset(
+              'assets/images/background.svg',
+              fit: BoxFit.cover,
+              height: 600,
+            ),
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 1.5, sigmaY: 1.5),
+            child: Container(color: Colors.white.withOpacity(.5)),
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: SvgPicture.asset(
+                    'assets/images/logo.svg',
+                    height: 150,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      'File UP을 이용하시려면 로그인이 필요합니다.\n로그인을 진행해주세요.',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    BaseAuthButton.google(),
+                    BaseAuthButton.apple(),
+                    BaseAuthButton.kakao(),
+                    BaseAuthButton.naver(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('FileUp이 처음이신가요?'),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            '회원가입',
+                            style: TextStyle(
+                              color: Color.fromRGBO(89, 130, 200, 1),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+    );
   }
 }
---
+++ lib/src/feature/presentation/pages/auth_page/local_widgets/base_auth_button.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+enum PlatformType {
+  google('구글', 'google_icon'),
+  apple('애플', 'apple_icon'),
+  kakao('카카오', 'kakao_icon'),
+  naver('네이버', 'naver_icon');
+
+  final String name;
+  final String path;
+
+  const PlatformType(this.name, this.path);
+}
+
+class BaseAuthButton extends StatelessWidget {
+  final PlatformType authType;
+
+  const BaseAuthButton.google({
+    Key? key,
+    this.authType = PlatformType.google,
+  }) : super(key: key);
+
+  const BaseAuthButton.apple({
+    Key? key,
+    this.authType = PlatformType.apple,
+  }) : super(key: key);
+
+  const BaseAuthButton.kakao({
+    Key? key,
+    this.authType = PlatformType.kakao,
+  }) : super(key: key);
+
+  const BaseAuthButton.naver({
+    Key? key,
+    this.authType = PlatformType.naver,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color backgroundColor = Colors.black;
+    Color textColor = Colors.black;
+
+    switch (authType) {
+      case PlatformType.google:
+        backgroundColor = Colors.white;
+        textColor = Colors.black;
+        break;
+      case PlatformType.apple:
+        backgroundColor = Colors.black;
+        textColor = Colors.white;
+        break;
+      case PlatformType.kakao:
+        backgroundColor = const Color.fromRGBO(254, 229, 0, 1);
+        textColor = Colors.black;
+        break;
+      case PlatformType.naver:
+        backgroundColor = const Color.fromRGBO(3, 199, 90, 1);
+        textColor = Colors.white;
+        break;
+    }
+
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 25),
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(backgroundColor),
+        ),
+        onPressed: () {},
+        child: Row(
+          children: [
+            SvgPicture.asset('assets/images/${authType.path}.svg'),
+            Expanded(
+              child: Center(
+                child:  Text(
+                  '${authType.name} 로그인',
+                  style: TextStyle(
+                    color: textColor,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
